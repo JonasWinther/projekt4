5281762dabd324ac053fbb0938e200963d001617
JonasWinther <jonas.f@bricius.dk>
2021-03-03 11:22:16 +0100

    index

---
+++ Afklaringsforløb/scripts/nyhamburger.js
@@ -0,0 +1,11 @@
+function opennav()
+{
+  document.getElementById("slidenav").style.width="175px";
+  document.getElementById("menuicon").style.display="none";
+
+}
+function closnav() {
+  document.getElementById("slidenav").style.width="0";
+  document.getElementById("menuicon").style.display="block";
+
+}
